dd53432082ae98731665bf273c11ff99eb0fd752
Soufiane Ezzine <ezzine.soufiane@gmail.com>
2022-09-16 17:34:16 +0900

    added data file with some feedback seeds and created the relavant componenets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
+import FeedbackData from './data/FeedbackData';
 import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
 
 export default function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
   return (
     <>
       <Header text={'FeedBack UI'} />
       <div className='container'>
-        <h1>my app</h1>
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   );

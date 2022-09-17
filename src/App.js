4e09c4a47fed33777d6f281a3ab2e6bc0acfa071
Soufiane Ezzine <ezzine.soufiane@gmail.com>
2022-09-17 14:41:11 +0900

    added form and hooked the input to the state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import FeedbackData from "./data/FeedbackData";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 
 export default function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -17,6 +18,7 @@ export default function App() {
     <>
       <Header text={"FeedBack UI"} />
       <div className="container">
+        <FeedbackForm />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>

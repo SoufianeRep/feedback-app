8485d2d297631ace1409b01197e1a65529092d57
Soufiane Ezzine <ezzine.soufiane@gmail.com>
2022-09-17 14:20:11 +0900

    added feedback stats functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
-import FeedbackData from './data/FeedbackData';
-import Header from './components/Header';
-import FeedbackList from './components/FeedbackList';
+import FeedbackData from "./data/FeedbackData";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
 
 export default function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
-  const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete?')) {
-      setFeedback(feedback.filter((item) => item.id !== id));
+  const deleteFeedback = id => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter(item => item.id !== id));
     }
   };
 
   return (
     <>
-      <Header text={'FeedBack UI'} />
-      <div className='container'>
+      <Header text={"FeedBack UI"} />
+      <div className="container">
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
